44bff531119b9e241099576790fd1c1cb3ff657c
Jwickster <joshwickster@gmail.com>
2020-10-09 12:10:17 -0600

    initial commit

---
+++ questions.js
@@ -0,0 +1,31 @@
+const questions = [{
+        title: "Question 1: The logical operator that represents 'AND' is ____.",
+        choices: ["||", "OR", "&&", "==="],
+        answer: "&&"
+    },
+
+    {
+        title: "Question 2: Javascript is considered an ____ language",
+        choices: ["interpreted", "compiled", "assembly", "binary"],
+        answer: "interpreted"
+    },
+
+    {
+        title: "Question 3: Every statement in Javascript must end with _____",
+        choices: [";", ".", ">", "<"],
+        answer: ";"
+    },
+
+    {
+        title: "Question 4: The instructions for a function are enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "curly brackets"
+    },
+
+    {
+        title: "Question 5: Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+
+    }
+];
